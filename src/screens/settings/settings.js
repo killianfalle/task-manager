4f639a95bbadf3468700f98aa5357b53059a11d3
killianfalle <killianfalle7@gmail.com>
2023-01-25 08:42:30 +0800

    add function to clear all data in settings page

--- src/screens/settings/settings.js
+++ src/screens/settings/settings.js
@@ -1,11 +1,23 @@
 import React, { useContext } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { Alert, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../../assets/styles/global-styles";
 import { Context } from "../../stores/context/context";
 import Toggle from "../../components/switch/switch";
 
 const Settings = () => {
-  const {settings, setSettings} = useContext(Context)
+  const {settings, setSettings, setTasks, setToast} = useContext(Context);
+
+  const showConfirmation = (title, description, action = () => {}) => {
+    Alert.alert(title, description, [
+        {text: 'OK', onPress: action},
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+      ],
+      {cancelable: false},
+    );
+  }
 
   const handleToggleSwitch = (type) => {
     if(type === "show-completed-tasks"){
@@ -17,10 +29,40 @@ const Settings = () => {
     }
   }
 
+  const handleAction = (type) => {
+    if(type === 'clear-data'){
+      showConfirmation(
+        "Confirm clear all data",
+        "You can't undo this after you cleared your data",
+        () => handleClearData());
+    }
+  }
+
+  const handleClearData = async() => {
+    asyncstorage.remove('tasks');
+    setTasks({
+      todo: {
+        title: 'To do',
+        data: [],
+      },
+      completed: {
+        title: 'Completed',
+        data: [],
+      }
+    });
+
+    setToast(prev => ({
+      ...prev,
+        visible: true,
+        title: 'Successfully cleared all data',
+        type: "primary"
+    }));
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.wrapper}>
-        <Text style={styles.pageTitle}>Settings Screen</Text>
+        <Text style={styles.pageTitle}>Settings</Text>
 
         <Toggle
           value={settings.showCompletedTasks}
@@ -35,6 +77,10 @@ const Settings = () => {
           label="Show Total Number of Tasks"
           description="Option to show/hide the total number of todo and completed tasks"
         />
+        
+        <TouchableOpacity onPress={() => handleAction('clear-data')} style={{marginVertical: 10}}>
+          <Text style={{fontSize: 20}}>Clear all data</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
